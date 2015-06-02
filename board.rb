74198153fdb89d3e2970d3ccb5833d8b259125dd
James + Dean <James+Dean@appacademy.io>
2015-06-02 12:19:26 -0400

    Create Board#initialize and [] sugars

--- board.rb
+++ board.rb
@@ -1,3 +1,16 @@
 class Board
   BOARD_SIZE = 8
+
+  def initialize
+    @board = Array.new(8) { Array.new(8) }
+  end
+
+  def [](*pos)
+    @board[pos.first][pos.last]
+  end
+
+  def []=(*pos, value)
+    @board[pos.first][pos.last] = value
+  end
+
 end
