f15c6c8ba542a5f3043835a8f38ab75ae3758144
James + Dean <James+Dean@appacademy.io>
2015-06-02 15:48:17 -0400

    Add Board#set_pieces

--- board.rb
+++ board.rb
@@ -8,6 +8,46 @@ class Board
     @board = Array.new(8) { Array.new(8) }
   end
 
+  def set_pieces
+    @board.each_with_index do |rank, rank_index|
+      case rank_index
+      when 0
+        set_powers(rank_index, :black)
+      when 1
+        set_pawns(rank_index, :black)
+      when 6
+        set_pawns(rank_index, :white)
+      when 7
+        set_pawns(rank_index, :white)
+      end
+    end
+  end
+
+  def set_pawns(rank_index, color)
+    BOARD_SIZE.times do |file_index|
+      pos = [rank_index, file_index]
+      self[*pos] = Pawn.new(self, color, pos)
+    end
+  end
+
+  def set_powers(rank_index, color)
+    BOARD_SIZE.times do |file_index|
+      pos = [rank_index, file_index]
+      case file_index
+      when 0, 7
+        self[*pos] = Rook.new(self, color, pos)
+      when 1, 6
+        self[*pos] = Knight.new(self, color, pos)
+      when 2, 5
+        self[*pos] = Bishop.new(self, color, pos)
+      when 3
+        self[*pos] = Queen.new(self, color, pos)
+      when 4
+        self[*pos] = King.new(self, color, pos)
+      end
+    end
+  end
+
   def [](*pos)
     @board[pos.first][pos.last]
   end
