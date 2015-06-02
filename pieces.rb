13724793b334af7ddc6af353a9562d5061c01a4b
James + Dean <James+Dean@appacademy.io>
2015-06-02 11:26:08 -0400

    Make pieces.rb with empty Piece classes

---
+++ pieces.rb
@@ -0,0 +1,35 @@
+class Piece
+
+end
+
+class SteppingPiece < Piece
+
+end
+
+class SlidingPiece < Piece
+
+end
+
+class Pawn < Piece
+
+end
+
+class Knight < SteppingPiece
+  
+end
+
+class King < SteppingPiece
+
+end
+
+class Bishop < SlidingPiece
+
+end
+
+class Rook < SlidingPiece
+
+end
+
+class Queen < SlidingPiece
+
+end
