35bb12292d9a1768531954d14229313d6c475357
James + Dean <james+dean@appacademy.io>
2015-06-03 12:00:39 -0400

    Create HumanPlayer#get_move, with #invalid_input? regexp check

--- chess.rb
+++ chess.rb
@@ -4,7 +4,7 @@ class Game
 
   def self.notation_lookup
     hash = Hash.new
-    ("a".."h").each_with_index do |letter, file|
+    ("A".."H").each_with_index do |letter, file|
       (1..8).reverse.each_with_index do |num, rank|
          hash[letter + num.to_s] = [rank,file]
       end
--- human_player.rb
+++ human_player.rb
@@ -1,16 +1,35 @@
 class HumanPlayer
 
-  
-  def initialize(color)
+
+  def initialize(color, board)
     @color = color
+    @board = board
   end
 
   def play_turn
 
-
   end
 
   private
+
+  def get_move
+    puts "Input a move (e.g. E2-E4)"
+    input = gets.chomp.upcase
+    if invalid_input?(input)
+      raise InvalidSelectionError.new("Bad input format.")
+    end
+
+    start_pos = Game::CHESS_NOTATION[input[0..1]]
+    end_pos = Game::CHESS_NOTATION[input[3..4]]
+    target = @board[*start_pos]
+
+    raise InvalidSelectionError if target.color != @color || target.nil?
+
+
+
+
+  end
+
   def pick_piece
 
   end
@@ -19,5 +38,12 @@ class HumanPlayer
 
   end
 
+  def invalid_input?(input)
+    !/\A[A-H][1-8]-[A-H][1-8]\z/.match(input)
+  end
 
+
+end
+
+class InvalidSelectionError < StandardError
 end
