7bbaef5b052a91b4e627ca3ea63c3946033fceb0
James + Dean <james+dean@appacademy.io>
2015-06-03 11:33:44 -0400

    Add Game::CHESS_NOTATION built with class method

--- chess.rb
+++ chess.rb
@@ -1,6 +1,17 @@
 class Game
 
+  TILE_NAME_PARSER = self.notation_lookup
 
+  def self.notation_lookup
+    hash = Hash.new
+    ("a".."h").each_with_index do |letter, file|
+      (1..8).reverse.each_with_index do |num, rank|
+         hash[letter + num.to_s] = [rank,file]
+      end
+    end
+
+    hash
+  end
 
   def initialize
     @board = Board.create_new_board
