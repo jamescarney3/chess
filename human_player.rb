7d5b5ccc1e09fefd06bdc888255f52eea9c78736
James + Dean <james+dean@appacademy.io>
2015-06-03 12:10:18 -0400

    Finish HumanPlayer#play_turn with error rescue

--- human_player.rb
+++ human_player.rb
@@ -7,6 +7,24 @@ class HumanPlayer
   end
 
   def play_turn
+    input = get_move
+
+    start_pos = Game::CHESS_NOTATION[input[0..1]]
+    end_pos = Game::CHESS_NOTATION[input[3..4]]
+    target = @board[*start_pos]
+
+    if target.nil?
+      raise InvalidSelectionError.new("No piece there.")
+    elsif target.color != @color
+      raise InvalidSelectionError.new("Not your piece.")
+    elsif !target.valid_moves.include?(end_pos)
+      raise InvalidSelectionError.new("Can't move there.")
+    end
+
+    @board.move(start_pos, end_pos)
+  rescue InvalidSelectionError => error
+    puts error.message
+    retry
 
   end
 
@@ -15,19 +33,12 @@ class HumanPlayer
   def get_move
     puts "Input a move (e.g. E2-E4)"
     input = gets.chomp.upcase
+
     if invalid_input?(input)
       raise InvalidSelectionError.new("Bad input format.")
     end
 
-    start_pos = Game::CHESS_NOTATION[input[0..1]]
-    end_pos = Game::CHESS_NOTATION[input[3..4]]
-    target = @board[*start_pos]
-
-    raise InvalidSelectionError if target.color != @color || target.nil?
-
-
-
-
+    input
   end
 
   def pick_piece
